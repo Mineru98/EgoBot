db6c4d6e97adb605d113ee886fbee4096ead522d
Mineru98 <rmstjr1030@naver.com>
2020-11-26 07:40:52 +0000

    Update Code

--- src/instagram/bot.py
+++ src/instagram/bot.py
@@ -1,14 +1,7 @@
-import io
 import os
-import sys
 import time
-from datetime import datetime
 
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 
 # dotenv init
