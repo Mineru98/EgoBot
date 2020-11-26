b1814695f8309f2b41a75fdc621ed9e10c4b61ec
Mineru98 <rmstjr1030@naver.com>
2020-11-26 07:39:31 +0000

    update code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # EgoBot
+
+```sh
+killall --quiet --signal 15 -- chrome
+killall --quiet --signal 15 -- chromedriver
+
+python src/instagram/bot.py
+
+```
---
+++ src/instagram/bot.py
@@ -0,0 +1,93 @@
+import io
+import os
+import sys
+import time
+from datetime import datetime
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+
+# dotenv init
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY_NAME = os.getenv("NAME")
+SECRET_KEY_PW = os.getenv("PW")
+
+###
+#
+# selenium setting
+#
+###
+_chrome_options = webdriver.ChromeOptions()
+_chrome_options.add_argument('disable-infobars')
+_chrome_options.add_argument('headless')
+_chrome_options.add_argument("lang=ko_KR")
+_chrome_options.add_argument('--no-sandbox')
+_chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('./chromedriver', options=_chrome_options)
+
+
+def login():
+    driver.get(
+        'https://www.instagram.com/accounts/login/?source=auth_switcher'
+    )
+    time.sleep(3)
+
+    id_input = driver.find_elements_by_css_selector(
+        '#loginForm > div > div:nth-child(1) > div > label > input'
+    )[0]
+    id_input.send_keys(SECRET_KEY_NAME)
+    password_input = driver.find_elements_by_css_selector(
+        '#loginForm > div > div:nth-child(2) > div > label > input'
+    )[0]
+    password_input.send_keys(SECRET_KEY_PW)
+    password_input.submit()
+    driver.save_screenshot("1.png")
+
+    # driver.find_element_by_xpath(
+    #     '//*[@id="react-root"]/section/main/div/div/div/div/button'
+    # ).click()
+
+    time.sleep(1)
+    driver.save_screenshot("2.png")
+
+
+def search(word):
+    driver.get('https://www.instagram.com/explore/tags/%s' % (word))
+    time.sleep(3)
+    driver.save_screenshot("tmp.png")
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    for link in soup.html.body.find_all("a"):
+        if 'href' in link.attrs:
+            print(link.attrs['href'])
+    time.sleep(1)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # 1초 대기
+    time.sleep(1)
+
+
+login()
+search('행복')
+driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
